7305ae5ec3045bd8358315c6b5dbbdb6a0a2d9c6
Narek Matosyan <narekovski17@gmail.com>
2023-01-19 15:46:20 +0400

    IOS-1: Created project

---
+++ Image gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image gallery
+//
+//  Created by Narek Matosyan on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
