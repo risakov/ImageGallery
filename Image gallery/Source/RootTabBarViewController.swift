46dcaccd877df68cd89ef78862b2e4217632cc22
Narek Matosyan <narekovski17@gmail.com>
2023-01-20 23:39:47 +0400

    ios-5-emptyStateFixed

--- Image gallery/Source/RootTabBarViewController.swift
+++ Image gallery/Source/RootTabBarViewController.swift
@@ -11,8 +11,6 @@ class RootTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.translatesAutoresizingMaskIntoConstraints = true
-
         // Do any additional setup after loading the view.
     }
     
