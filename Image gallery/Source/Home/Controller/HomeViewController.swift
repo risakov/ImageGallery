fe40636ae2f4890c2f7b9528a7624ce12ed786d5
Narek Matosyan <narekovski17@gmail.com>
2023-01-24 02:21:10 +0400

    mergeDevelop

--- Image gallery/Source/Home/Controller/HomeViewController.swift
+++ Image gallery/Source/Home/Controller/HomeViewController.swift
@@ -8,26 +8,13 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    @IBOutlet weak var newView: UIView!
-    @IBOutlet weak var popularView: UIView!
+
     @IBOutlet weak var emptyStateStackView: UIStackView!
     
-    @IBAction func choiceSegment(_ sender: UISegmentedControl) {
-        if  sender.selectedSegmentIndex == 0 {
-            popularView.alpha = 1
-            newView.alpha = 0
-        } else {
-            popularView.alpha = 0
-            newView.alpha = 1
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.title = "Gallery"
-        emptyStateStackView.isHidden = true
-        let attributes: [NSAttributedString.Key : Any] = [NSAttributedString.Key.font: UIFont(name: "Roboto-Regular", size: 17)!]
-        UISegmentedControl.appearance().setTitleTextAttributes(attributes, for: .normal)
         
+        emptyStateStackView.isHidden = true
     }
 }
